ea8622ef21a27bcbc396d06beed2e5852ae52075
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-30 10:07:38 +0530

    Added HashTable

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using HashTableUC1;
+
+MyMapNode<string, string> a = new MyMapNode<string, string>(5);
+a.Add("0", "To");
+a.Add("1", "be");
+a.Add("2", "or");
+a.Add("3", "not");
+a.Add("4", "to");
+a.Add("5", "be");
+
+string a5 = a.Get("5");
+Console.WriteLine("5th index value : " + a5);
